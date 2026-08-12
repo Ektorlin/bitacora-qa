4a25eeefdb317d22ee234c28cbc9491aa4a795e4
Ektorlin <hpleanos@gmail.com>
2026-08-12 07:28:47 -0600

    ejemplo reto3 gherkin

---
+++ codecept.conf.js
@@ -0,0 +1,18 @@
+/** @type {CodeceptJS.MainConfig} */
+export const config = {
+  tests: '**./*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'http://localhost',
+      show: true
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  noGlobals: true,
+  plugins: {},
+  name: 'bitacora-qa'
+}
